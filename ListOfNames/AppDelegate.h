ecce72afdc89649f0e4b2b081dd3cb3eafee7f52
Aleh Pachtovy <alehpachtovy@Alehs-Mac-mini.local>
2018-08-26 18:19:51 +0300

    Initial commit

---
+++ ListOfNames/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ListOfNames
+//
+//  Created by Aleh Pachtovy on 8/26/18.
+//  Copyright © 2018 Aleh Pachtovy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ListOfNames/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ListOfNames
+//
+//  Created by Aleh Pachtovy on 8/26/18.
+//  Copyright © 2018 Aleh Pachtovy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
